13d6eb8c57384a3c16ad2844eafdbb0fd1b025f9
HojinLim <69897998+HojinLim@users.noreply.github.com>
2022-08-20 22:43:51 +0900

    Update MainActivity.kt

--- app/src/main/java/com/example/gitproject/MainActivity.kt
+++ app/src/main/java/com/example/gitproject/MainActivity.kt
@@ -7,5 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //Just for Test from git web
     }
 }
