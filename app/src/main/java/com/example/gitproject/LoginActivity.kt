85ed23433da2d1c02e780c0d2aadb4466ec37887
HojinLim <69897998+HojinLim@users.noreply.github.com>
2022-08-22 21:25:14 +0900

    로그인, 회원가입 ui 끄적이기

--- app/src/main/java/com/example/gitproject/LoginActivity.kt
+++ app/src/main/java/com/example/gitproject/LoginActivity.kt
@@ -1,13 +1,14 @@
 package com.example.gitproject
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
 
+        
 
     }
 }
