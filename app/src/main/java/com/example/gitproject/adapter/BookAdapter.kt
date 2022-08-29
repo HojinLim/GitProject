765eab64db3afb012ea76af04a2cfa159fb8d444
HojinLim <69897998+HojinLim@users.noreply.github.com>
2022-08-29 19:09:07 +0900

    검색기능 완료

--- app/src/main/java/com/example/gitproject/adapter/BookAdapter.kt
+++ app/src/main/java/com/example/gitproject/adapter/BookAdapter.kt
@@ -5,6 +5,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.gitproject.databinding.ItemBookBinding
 import com.example.gitproject.model.Book
 
@@ -12,6 +13,13 @@ class BookAdapter: ListAdapter<Book, BookAdapter.BookItemViewHolder>(diffUtil) {
     inner class BookItemViewHolder(private val binding: ItemBookBinding): RecyclerView.ViewHolder(binding.root){
         fun bind(bookModel: Book){
             binding.titleTextView.text= bookModel.title
+            binding.descriptionTextView.text= bookModel.description
+
+
+            Glide
+                .with(binding.coverImageView.context)
+                .load(bookModel.coverSmallUrl)
+                .into(binding.coverImageView)
         }
     }
 
